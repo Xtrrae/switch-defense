6e3d5606ba6a13e1f49dccc138d2c1f4f0e6718d
Ajitesh Kumar <ajinest6@gmail.com>
2026-02-21 10:13:50 -0800

    rebound on collision with input box/light switches

--- switch-defence/scenes/points.gd
+++ switch-defence/scenes/points.gd
@@ -1,11 +1,15 @@
 extends Node2D
 
-var points = 0;
+var points = 1000;
 var level = 1;
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	# 1.9x^2 + 12 = points
+	# (point - 12)/1.9 = x^2
+	level = sqrt((points - 12)/1.9)
+	$PointsLabel.text = "%.0d" % points
+	$LevelLabel.text = "%d" % level
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
--- switch-defence/scripts/monster.gd
+++ switch-defence/scripts/monster.gd
@@ -1,11 +1,14 @@
 extends CharacterBody2D
 
-var destruction_combination = [false, false, false, false];
+var destruction_combination: Array[bool] = [false, false, false, false];
 var type = ""; # attack_focused | speed_focused
 var speed = 0;
 var attack = 0;
 
-const target = Vector2(576, 324);
+var starting_position = Vector2(0, 0)
+
+var target
+var in_rebound = false;
 
 @onready var navigation_agent: NavigationAgent2D = $NavigationAgent2D
 
@@ -14,11 +17,16 @@ func _ready() -> void:
 	# choose a random combination and update the label
 	for i in range(4):
 		destruction_combination[i] = randi_range(0, 1) == 0
-	if destruction_combination == [0, 0, 0, 0]: # 0,0,0,0 shouldn't be an option
+	if destruction_combination == [false, false, false, false]: # 0,0,0,0 shouldn't be an option
 		destruction_combination[randi_range(0, 3)] = 1 # if it's 0,0,0,0 -> set a random digit to 1
 	var dc_label: Label = get_node("DefeatCombinationLabel")
 	dc_label.text = "%d%d%d%d" % destruction_combination.map(func (x): if x: return 1 else: return 0)
 	actor_setup()
+	
+	# set target
+	#target = Vector2(576, 324);
+	#target = +/- 110, +/- 50
+	target = Vector2(576 + randi_range(-110, 110), 324 + randi_range(-50, 50))
 
 func actor_setup():
 	# Wait for the first physics frame so the NavigationServer can sync.
@@ -30,17 +38,27 @@ func actor_setup():
 func set_movement_target(movement_target: Vector2):
 	navigation_agent.target_position = movement_target
 
-func _physics_process(_delta):
-	if navigation_agent.is_navigation_finished():
-		return
-
+func _physics_process(delta):
 	var current_agent_position: Vector2 = global_position
 	var next_path_position: Vector2 = navigation_agent.get_next_path_position()
-
-	velocity = current_agent_position.direction_to(next_path_position) * speed * 1000
 	
+	var effective_speed = speed
+	if in_rebound:
+		effective_speed *= 6.7
+	
+	velocity = (next_path_position - current_agent_position) * effective_speed
+	
+	if navigation_agent.is_target_reached() and in_rebound:
+		in_rebound = false
+		print("rebound done, target: ", target)
+		set_movement_target(target)
+		
 	move_and_slide()
 
+func rebound() -> void:
+	const REBOUND_FRACTION = 0.5
+	in_rebound = true
+	set_movement_target(position + (starting_position - position) * REBOUND_FRACTION)
 
 func destroy() -> void:
 	pass
