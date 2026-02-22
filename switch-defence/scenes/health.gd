ff13669ad5de4bb717486ad4f90840b1e6256533
Ajitesh Kumar <ajinest6@gmail.com>
2026-02-22 11:19:43 -0800

    add end and beginning screens that work and create a little thing

--- switch-defence/scenes/health.gd
+++ switch-defence/scenes/health.gd
@@ -15,4 +15,10 @@ func change_health(d: float) -> void:
 	health -= d
 	value = health
 	if value <= 0:
-		get_tree().call_deferred("change_scene_to_file", "res://scenes/end.tscn")
+		var next_scene = load("res://scenes/end.tscn").instantiate()
+		next_scene.ending_points = $"../Points".points
+		next_scene.ending_level = $"../Points".level
+		
+		get_tree().root.add_child(next_scene)
+		get_tree().current_scene.queue_free()
+		get_tree().current_scene = next_scene
